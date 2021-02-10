56794580a38778543365e4e2bd3ca0389614f95b
Sandra To <sandra.to.e@gmail.com>
2021-02-10 21:48:05 +0000

    Refactored and export input and connection module to play file

--- play.js
+++ play.js
@@ -1,31 +1,14 @@
+const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 /**
  * Establishes connection with the game server
  */
-const connect = require('./client');
-
 console.log('Connecting ...');
-
 connect();
 
 /**
  * Setup User Interface
  * Specifically, so that we can handle user input via stdin
  */
-
-
-const handleUserInput = key => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
-
 setupInput();
