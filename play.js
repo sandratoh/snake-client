2e0ee09370f959ba51650bff570a34af4b33a90e
Sandra To <sandra.to.e@gmail.com>
2021-02-10 21:42:41 +0000

    stdin setup codes added

--- play.js
+++ play.js
@@ -5,4 +5,27 @@ const connect = require('./client');
 
 console.log('Connecting ...');
 
-let client = connect();
+connect();
+
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+
+
+const handleUserInput = key => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+setupInput();
